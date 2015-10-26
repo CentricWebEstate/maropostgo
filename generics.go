292f119e6f3dee174f58af09437dbb2a7664488e
Daniel J Holmes (jaitaiwan) <dan@centricwebestate.com>
2015-10-26 22:44:09 +1000

    Small debuging

--- generics.go
+++ generics.go
@@ -4,10 +4,12 @@ import "sync"
 import "encoding/json"
 import "net/http"
 import "bytes"
+import "fmt"
 
 func MakeRequest(address string, method string, data interface{}) (*http.Response, error) {
 	address = API_URL + address
 	jsob, err := json.Marshal(data)
+	fmt.Print("%v\n", jsob)
 	if err != nil {
 		return nil, err
 	}
